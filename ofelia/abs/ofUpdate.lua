f9731978e1a7b3e8bba4b01810916322a1e97c73
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-04-10 16:32:29 +0200

    Add files via upload

---
+++ ofelia/abs/ofUpdate.lua
@@ -0,0 +1,35 @@
+local log = ofLog("ofUpdate")
+local canvas = ofCanvas(this)
+local order, enabled = 50, true
+
+function M.new()
+  local args = canvas:getArgs()
+  if #args == 1 then
+    order = args[1]
+  elseif #args == 2 then
+    order, enabled = args[1], args[2] ~= 0
+  elseif #args ~= 0 then
+    log:error("requires 1 or 2 creation arguments")
+  end
+  ofWindow.addListener("update", this, order)
+end
+
+function M.setOrder(f)
+  order = f
+  ofWindow.addListener("update", this, order)
+end
+
+function M.setEnabled(b)
+  enabled = b ~= 0
+end
+
+function M.free()
+  ofWindow.removeListener("update", this)
+end
+
+function M.update()
+  if not enabled then
+    return
+  end
+  return nil
+end
